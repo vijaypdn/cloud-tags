f2f69fed99d9cc6a6ba9e3832cddf278436591cd
vijaypdn <vijaypdn@inotous.com>
2013-02-22 10:09:43 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudTags::Application.config.secret_token = '56956d568e06f32a74bd53ce29b58ee186ba34eef127f7546412b3863a9afd22f77f4c46f284046a7af7bc1b899c9fffc8916c4d7ce82e70709e77b38d4951fb'
